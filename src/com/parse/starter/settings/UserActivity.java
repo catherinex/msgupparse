543ced17d8d43c2d7c45681d0bd5fd1133c32c48
catherinex <demonxuxiang@gmail.com>
2014-12-24 18:22:16 +0100

    implement UserActivity

--- src/com/parse/starter/settings/UserActivity.java
+++ src/com/parse/starter/settings/UserActivity.java
@@ -1,18 +1,74 @@
 package com.parse.starter.settings;
 
+import com.parse.ParseUser;
 import com.parse.starter.R;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Point;
 import android.os.Bundle;
+import android.view.Display;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
 
 public class UserActivity extends Activity {
+	
+	private ImageView ivIcon, ivEditIcon, ivEditNickname;
+	private EditText etNickname;
+	private static final int SELECT_PHOTO = 100;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_user);
+		
+		ParseUser user = ParseUser.getCurrentUser();
+		
+		ivIcon = (ImageView)findViewById(R.id.iv_user_icon);
+		ivEditIcon = (ImageView)findViewById(R.id.iv_user_edit_icon);
+		ivEditNickname = (ImageView)findViewById(R.id.iv_user_edit_nickname);
+		etNickname = (EditText)findViewById(R.id.et_user_nickname);
+		
+		// set widgets
+		Display display = getWindowManager().getDefaultDisplay();
+		Point size = new Point();
+		display.getSize(size);
+		int width = size.x;
+		int height = size.y;
+		ivIcon.getLayoutParams().height = width - 16 - 16;
+		if (user.getString("nickname") != null)
+			etNickname.setText(user.getString("nickname"));
+		
+		// widgets actions
+		ivEditIcon.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				onClickIconEditButton();
+			}
+		});
+		
+		ivEditNickname.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				onClickNameEditButton();
+			}
+		});
+	}
+	
+	private void onClickIconEditButton() {
+		Intent photoPickerIntent = new Intent(Intent.ACTION_PICK);
+		photoPickerIntent.setType("image/*");
+		startActivityForResult(photoPickerIntent, SELECT_PHOTO);    
+	}
+	
+	private void onClickNameEditButton() {
+		Intent intent = new Intent(this, NameActivity.class);
+		startActivity(intent);
 	}
 
 	@Override
